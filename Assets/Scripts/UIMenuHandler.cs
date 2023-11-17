030f58ae8028b3747ea305db4b21a829ba5d0420
JuanMarGam <99370854+JuanMarGam@users.noreply.github.com>
2023-11-16 23:21:26 -0500

    UI and Menus done

--- Assets/Scripts/UIMenuHandler.cs
+++ Assets/Scripts/UIMenuHandler.cs
@@ -2,38 +2,106 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIMenuHandler : MonoBehaviour
 {
-    public string mainMenu = "Main Menu";
-    public string settingsMenu = "Settings Menu";
-    public string mainSettingsMenu = "Main Settings Menu";
-    public string pauseMenu = "Pause Menu";
+    public GameObject MainMenuUI;
+    public GameObject SettingsUI;
+    public GameObject CreditsUI;
+    public Image musicButton;
+    public Sprite musicOn;
+    public Sprite musicOff;
+    public Image efectsButton;
+    public Sprite efectsOn;
+    public Sprite efectsOff;
+    public Image GODModeButton;
+    public Sprite ModeOn;
+    public Sprite ModeOff;
+    public Image NormalModeButton;
+    private bool isMusicOn;
+    private bool isEfectsOn;
+    private bool isNormalModeOn;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        MainMenuUI.SetActive(true);
+        SettingsUI.SetActive(false);
+        CreditsUI.SetActive(false);
+        isMusicOn = true;
+        isEfectsOn = true;
+        NormalModeButton.sprite = ModeOn;
+        isNormalModeOn = true;
+}
 
     // Update is called once per frame
     void Update()
     {
         
     }
-    public void ChangeToMainMenu()
+    public void NormalModeHandler()
     {
-        SceneManager.LoadScene(mainMenu);
+        if (isNormalModeOn)
+        {
+            NormalModeButton.sprite = ModeOff;
+            GODModeButton.sprite = ModeOn;
+            isNormalModeOn = false;
+        }
+        else
+        {
+            NormalModeButton.sprite = ModeOn;
+            GODModeButton.sprite = ModeOff;
+            isNormalModeOn = true;
+        }
     }
-    public void ChangeToSettingsMenu()
+    public void MusicButtonHandler()
     {
-        SceneManager.LoadScene(settingsMenu);
+        if (isMusicOn)
+        {
+            musicButton.sprite = musicOff;
+            isMusicOn = false;
+        }
+        else
+        {
+            musicButton.sprite = musicOn;
+            isMusicOn = true;
+        }
     }
-    public void ChangeToMainSettingsMenu()
+    public void EfectsButtonHandler()
     {
-        SceneManager.LoadScene(mainSettingsMenu);
+        if (isEfectsOn)
+        {
+            efectsButton.sprite = efectsOff;
+            isEfectsOn = false;
+        }
+        else
+        {
+            efectsButton.sprite = efectsOn;
+            isEfectsOn = true;
+        }
     }
-    public void ChangeToPauseMenu()
+    public void DisplayMainMenuUI()
     {
-        SceneManager.LoadScene(pauseMenu);
+        MainMenuUI.SetActive(true);
+    }
+    public void CloseMainMenuUI()
+    {
+        MainMenuUI.SetActive(false);
+    }
+    public void DisplayMainSettingsUI()
+    {
+        SettingsUI.SetActive(true);
+    }
+    public void CloseMainSettingsUI()
+    {
+        SettingsUI.SetActive(false);
+    }
+    public void DisplayCreditsUI()
+    {
+        CreditsUI.SetActive(true);
+    }
+    public void CloseCreditsUI()
+    {
+        CreditsUI.SetActive(false);
     }
 }
